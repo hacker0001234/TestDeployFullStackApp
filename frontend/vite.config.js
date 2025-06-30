d13c28e0a9ae5f99dcbe424fa8f7b57d1e2a0fac
Mykola <mykola.chichkalenko@gmail.com>
2025-06-30 22:11:12 +0300

    change some files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: path.resolve(__dirname, 'TestDeploy/src/main/resources/static'), // або відповідно до твоєї структури
+    emptyOutDir: true
+  }
 })
